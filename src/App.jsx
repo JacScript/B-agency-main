636aa656ececd49f1c2432e6af17a125b66c3f0a
JacScript <jacksonsylas59@gmail.com>
2024-03-18 01:26:17 +0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     
   return (
     <>
-      <Home/>
-      {/* <Market/> */}
+      {/* <Home/> */}
+      <Market/>
     </>
   )
 }
--- src/components/ContainerMarket.jsx
+++ src/components/ContainerMarket.jsx
@@ -7,15 +7,15 @@ const ContainerMarket = () => {
   const handleClick = () => setIsOpen(!isOpen);
 
   return (
-    <div className="max-w-screen-2xl h-screen md:bg-banner-pattern md:bg-cover">
-      <div className=" md:relative px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-full md:bg-gradient-to-br from-green-950 to-tranparent bg-none">
+    <div className="max-w-screen-2xl h-screen bg-banner-pattern bg-cover">
+      <div className=" md:relative px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-full bg-gradient-to-br from-green-950 to-tranparent">
         <div className="mt-20 text-center">
           <h1 className="text-white text-4xl uppercase font-semibold pt-12">
             Select a product to Buy/Sell
           </h1>
         </div>
 
-        <div className="w-4/5 mx-auto flex justify-around items-center h-48 pt-4 border-b-2 border-brandPrimary">
+        <div className="md:w-4/5 w-full md:mx-auto lg:flex  grid grid-cols-2 gap-4 px-12 md:px-24 lg:px-0 lg:justify-around items-center h-48 pt-4 border-b-2 border-brandPrimary">
           <div>
             <button
               className="btn-primary flex items-center px-12"
