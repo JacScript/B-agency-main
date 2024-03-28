4aa1dbdd1374156e3986507394ed508c71ffdcf2
JacScript <jacksonsylas59@gmail.com>
2024-03-28 06:37:28 +0300

    minor changes

--- src/components/Conversion-component.jsx
+++ src/components/Conversion-component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Conversion = () => {
   return (
     <div className="max-w-screen-2xl h-screen mt-20">
-      <div className="w-3/6 mx-auto h-full flex flex-col">
+      <div className="md:w-full md:px-8  lg:w-4/6 lg:mx-auto h-full flex flex-col xl:w-3/6 xl:mx-auto">
         <div className="w-full h-32">
           <h1 className="kode-mono text-brandPrimary text-5xl text-center font-semibold">
             Crop Converter
@@ -11,8 +11,8 @@ const Conversion = () => {
         </div>
 
         <div className="w-full px-12">
-          <div className="flex items-center w-full">
-            <div className="mr-4 w-1/6 h-full">Product </div>
+          <div className="flex md:flex-row items-center w-full flex-col">
+            <div className="md:mr-4 w-1/6 h-full">Product </div>
 
             <div className="w-5/6">
               <select
@@ -41,12 +41,12 @@ const Conversion = () => {
             </div>
           </div>
 
-          <div className="w-full flex justify-between items-center mt-4">
-            <div className="w-1/2 flex">
-              <div className="w-1/3 mr-[1.8rem] flex items-center">
+          <div className="w-full flex justify-between items-center mt-4 md:flex-row flex-col">
+            <div className="w-full md:w-1/2 flex md:flex-row flex-col">
+              <div className="md:w-1/3 mx-auto md:mx-0 md:mr-[1.8rem] flex items-center ">
                 Price/Unit
               </div>
-              <div className="w-2/3 h-full">
+              <div className="w-5/6 mx-auto md:w-2/3 md:mx-0 h-full">
                 {/* <input
                   type="hidden"
                   id="real-price-unit-calculator"
@@ -97,7 +97,13 @@ const Conversion = () => {
                       </div>
                     </label>
                   </div>
+
+                
+
                 </div>
+
+               
+
               </div>
             </div>
           </div>
@@ -105,7 +111,7 @@ const Conversion = () => {
           <div className="w-full flex justify-between items-center mt-4">
             <div className="w-1/2 flex">
               <div className="w-1/3 mr-[1.8rem] flex items-center">
-                Price/Unit
+                Quality
               </div>
               <div className="w-2/3 h-full">
                 {/* <input
@@ -118,7 +124,6 @@ const Conversion = () => {
                   type="number"
                   name="price-unit-calculator"
                   id="price-unit-calculator"
-                  class="form-control crop-input"
                   step="0.1"
                   className="w-full h-full rounded-lg"
                 />
@@ -144,6 +149,151 @@ const Conversion = () => {
               </div>
             </div>
           </div>
+
+          <div className="flex items-center w-full mt-4">
+            <div className="mr-4 w-1/6 h-full">Total Price </div>
+
+            <div className="w-5/6">
+            <input
+                  type="number"
+                  name="calculated-price"
+                  id=""
+                  value="total-price"
+                  className="w-full h-full rounded-lg"
+                  disabled
+                />
+            </div>
+          </div>
+
+          <div className='w-full border-b-2 border-slate-300 my-8'></div>
+
+          <div className="w-full flex justify-between items-center mt-4">
+            <div className="w-1/2 flex">
+              <div className="w-1/3 mr-[1.8rem] flex items-center">
+                Price/Unit
+              </div>
+              <div className="w-2/3 h-full">
+                {/* <input
+                  type="hidden"
+                  id="real-price-unit-calculator"
+                  name="real-price-unit-calculator"
+                  value="-1"
+                /> */}
+                <input
+                  type="number"
+                  name="price-unit-calculator"
+                  id="price-unit-calculator"
+                  disabled
+                  className="w-full h-full rounded-lg"
+                />
+              </div>
+            </div>
+
+            <div className="ml-8 w-1/2">
+              <div className="w-full h-full flex justify-between">
+                <div className="w-2/5">Currency</div>
+                <div className="w-3/5 flex justify-between">
+                  <div className="w-1/2">
+                    <label className="">
+                      <div>
+                        <input
+                          id="calculator-btn-euro"
+                          type="radio"
+                          name="calculator-service"
+                          className="w-6 h-6 mr-1"
+                        />
+                        <span className="checkmark radio-span"></span>
+                        Euro
+                      </div>
+                    </label>
+                  </div>
+
+                  <div className="w-1/2">
+                    <label className="currency-container">
+                      <div>
+                        <input
+                          id="calculator-btn-usd"
+                          type="radio"
+                          name="calculator-service"
+                          className="w-6 h-6 mr-1"
+                        />
+                        <span className=""></span>
+                        USD
+                      </div>
+                    </label>
+                  </div>
+
+                
+
+                </div>
+
+               
+
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-between items-center mt-4">
+            <div className="w-1/2 flex">
+              <div className="w-1/3 mr-[1.8rem] flex items-center">
+                Quality
+              </div>
+              <div className="w-2/3 h-full">
+                {/* <input
+                  type="hidden"
+                  id="real-price-unit-calculator"
+                  name="real-price-unit-calculator"
+                  value="-1"
+                /> */}
+                <input
+                  type="number"
+                  name="price-unit-calculator"
+                  id="price-unit-calculator"
+                  disabled
+                  className="w-full h-full rounded-lg"
+                />
+              </div>
+            </div>
+
+            <div className="ml-8 w-1/2">
+              <div className="w-full h-full flex justify-between">
+                <div className="w-2/5 flex items-center">Unit</div>
+                <div className="w-3/5 flex justify-between ">
+                  <div className="w-1/2">
+                    <select
+                      name="unit-calculations"
+                      id="unit-calculations"
+                      className="rounded-lg"
+                    >
+                      <option value="001">Metric Ton</option>
+                      <option value="001">Pound</option>
+                      <option value="001">Kilograms</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center w-full mt-4">
+            <div className="mr-4 w-1/6 h-full">Total Price </div>
+
+            <div className="w-5/6">
+            <input
+                  type="number"
+                  name="calculated-price"
+                  id=""
+                  value="total-price"
+                  className="w-full h-full rounded-lg"
+                  disabled
+                />
+            </div>
+          </div>
+
+          <div className='w-full flex justify-center mt-8 items-center'>
+            <button className='btn-primary'>Convert</button>
+          </div>
+
         </div>
       </div>
     </div>
