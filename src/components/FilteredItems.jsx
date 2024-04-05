699b0e1d0d7bb8778d30aa7f451f8b9a1d65acf4
JacScript <jacksonsylas59@gmail.com>
2024-04-05 13:24:32 +0300

    minor changes

--- src/components/FilteredItems.jsx
+++ src/components/FilteredItems.jsx
@@ -4,7 +4,7 @@ import maize from '../assets/corn.jpg';
 
 const FilteredItems = () => {
   return (
-    <div className="mt-[4.6rem] max-w-screen-2xl h-dvh">
+    <div className="mt-[4.6rem] max-w-screen-2xl">
       <div className="w-5/6 mx-auto mt-24 flex">
         <div className="w-1/4 mt-12 flex flex-col">
           <div className="kode-mono text-2xl font-extrabold uppercase">
@@ -30,7 +30,7 @@ const FilteredItems = () => {
               <div className="bg-yellow-500">
                 <div className="mx-2">
                   <div className="flex flex-col items-center">
-                    <label className="text-center" for="name">
+                    <label className="text-center mb-1" for="name">
                       Product Status
                     </label>
                     <select
@@ -47,7 +47,7 @@ const FilteredItems = () => {
 
                 <div className="mx-2 mt-4">
                   <div className="flex flex-col items-center">
-                    <label className="text-center" for="name">
+                    <label className="text-center mb-1" for="name">
                       Product Type
                     </label>
                     <select
@@ -64,7 +64,7 @@ const FilteredItems = () => {
 
                 <div className="mx-2 mt-4">
                   <div className="flex flex-col">
-                    <label className="text-center" for="name">
+                    <label className="text-center mb-1" for="name">
                       Variety
                     </label>
                     <select
@@ -101,7 +101,7 @@ const FilteredItems = () => {
 
                 <div className="mx-2 mt-4">
                   <div className="flex flex-col">
-                    <label className="text-center" for="name">
+                    <label className="text-center mb-1" for="name">
                       Variety Style
                     </label>
                     <select
@@ -118,7 +118,7 @@ const FilteredItems = () => {
 
                 <div className="mx-2 mt-4">
                   <div className="flex flex-col">
-                    <label className="text-center" for="name">
+                    <label className="text-center mb-1" for="name">
                       Production Monde
                     </label>
                     <select
@@ -132,6 +132,179 @@ const FilteredItems = () => {
                       <option value="466">Conventional Production</option>
                     </select>
                   </div>
+                </div>
+
+                <div className="mx-2 mt-4">
+                  <div className="flex flex-col">
+                    <label className="text-center mb-1" for="name">
+                      US Size
+                    </label>
+                    <select
+                      name="search[128]"
+                      id="search_128"
+                      className="rounded-lg w-full"
+                    >
+                      <option value="">All</option>
+                      <option value="474">18/20</option>
+                      <option value="475">16/18</option>
+                      <option value="473">20/22</option>
+                      <option value="776">22/24</option>
+                      <option value="777">24/26</option>
+                      <option value="778">26/28</option>
+                      <option value="779">28/30</option>
+                      <option value="780">30/32</option>
+                      <option value="781">32/34</option>
+                      <option value="782">34/36</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="mx-2 mt-4">
+                  <div className="flex flex-col">
+                    <label className="text-center mb-1" for="name">
+                      Packaging for Shipment
+                    </label>
+                    <select
+                      name="search[134]"
+                      id="search_134"
+                      className="rounded-lg w-full"
+                    >
+                      <option value="">All</option>
+                      <option value="481">25 LBS (11.3 KG)</option>
+                      <option value="482">50 LBS (22.7 KG)</option>
+                      <option value="483">1000 LBS (454 KG)</option>
+                      <option value="484">1500 LBS (681 KG)</option>
+                      <option value="488">Big Bags 1000 KG</option>
+                      <option value="486">Cases 10 KG</option>
+                      <option value="487">Bags 25 KG</option>
+                      <option value="490">
+                        Other Packaging Size (Specify in Comments)
+                      </option>
+                      <option value="570">Big Bags 800 KG</option>
+                      <option value="489">
+                        In Bulk (Specify Metric Tons in Comments)
+                      </option>
+                      <option value="569">Cases 15 KG</option>
+                      <option value="485">2200 LBS (1 Metric Ton)</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div id="advanced_characteristics" className="my-4 hidden">
+                  <div className="mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        USDA Grades{" "}
+                      </label>
+                      <select
+                        name="search[127]"
+                        id="search_127"
+                        className="rounded-lg w-full"
+                      >
+                        <option value="">All</option>
+                        <option value="467">US Non-Split</option>
+                        <option value="468">Artificially Opened</option>
+                        <option value="469">US Select</option>
+                        <option value="470">US N. 1</option>
+                        <option value="471">US Extra N. 1</option>
+                        <option value="472">US Fancy</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="mt-4 mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        Europe Size (nuts per 100gr)
+                      </label>
+                      <select
+                        name="search[129]"
+                        id="search_129"
+                        className="rounded-lg w-full"
+                      >
+                        <option value="">All</option>
+                        <option value="480">Small (over 106)</option>
+                        <option value="479">Medium (92 to 106)</option>
+                        <option value="478">Large (81 to 91)</option>
+                        <option value="477">Very Large (71 to 80)</option>
+                        <option value="476">Extra Large (under 71)</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="mt-4 mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        Crop Year
+                      </label>
+                      <input
+                        type="number"
+                        name="search[189]"
+                        id="search_189"
+                        className="w-full rounded-lg"
+                      />
+                    </div>
+                  </div>
+
+
+                  <div className="mt-4 mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        Total Protein (gr/100gr){" "}
+                      </label>
+                      <input
+                        type="number"
+                        name="search[130]"
+                        id="search_130"
+                        className="w-full rounded-lg"
+                      />
+                    </div>
+                  </div>
+
+
+                  <div className="mt-4 mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        Moisture (Max.%){" "}
+                      </label>
+                      <input
+                        type="number"
+                        name="search[131]"
+                        id="search_131"
+                        className="w-full rounded-lg"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="mt-4 mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        Foreign matter (Max. %)
+                      </label>
+                      <input
+                        type="number"
+                        name="search[132]"
+                        id="search_132"
+                        className="w-full rounded-lg"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="mt-4 mx-2">
+                    <div className="flex flex-col">
+                      <label className="text-center mb-1" for="name">
+                        Defects (Max. %)
+                      </label>
+                      <input
+                        type="number"
+                        name="search[133]"
+                        id="search_133"
+                        className="rounded-lg w-full"
+                      />
+                    </div>
+                  </div>
+
+
                 </div>
               </div>
             </form>
