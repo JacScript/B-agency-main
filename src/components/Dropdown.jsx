9086c1822353e9c7693722922f50447e5e61712c
JacScript <jacksonsylas59@gmail.com>
2024-05-16 11:01:57 +0300

    minor adjustment on navbar

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
+import { Link } from "react-scroll";
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownitems = [
+    { link: "Our Solution", path: "solution" },
+    { link: "How It Works", path: "works" },
+    { link: "Get started", path: "start" },
+    { link: "Services", path: "services" },
+    { link: "Features", path: "features" },
+  ];
+
+  return (
+    <div className="relative flex flex-col items-center justify-center w-[200px] h-[24px] py-2 rounded-lg">
+      <button
+        className="p-2  w-full  flex items-center justify-between text-base rounded-lg tracking-wider border-4 border-transparent active:border-t-brandPrimary duration-300 active:text-white"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        Dropdown
+        {!isOpen ? (
+          <AiOutlineCaretDown className="h-8" />
+        ) : (
+          <AiOutlineCaretUp className="h-8" />
+        )}
+      </button>
+
+      {isOpen && (
+        <div className="bg-brandPrimary absolute top-14 flex flex-col items-start rounded-lg p-2 w-full">
+          {dropdownitems.map((item, path) => (
+            <ul className="italic p-2 ">
+              <Link to={item.path} spy={true} smooth={true} offset={-100} className=" hover:text-white text-bold cursor-pointer">{item.link}</Link>
+             </ul>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import Button from './Button'
 
 //react icons
 import {FaXmark, FaBars } from "react-icons/fa6"
+import Dropdown from './Dropdown';
  
 
 
@@ -52,20 +53,27 @@ const Navbar = () => {
      <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">  
       <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border bg-white duration-300" : "sticky top-0 left-0 right-0 border bg-white duration-300 "} `}>
         <div className="flex justify-between items-center text-base gap-8">
+          <div>
           <a href="" className='text-2xl font-semibold flex items-center space-x-3' ><img src={logo} alt="" className='w-10 inline-block items-center'/><span className='text-[#263238]'>AGENCY</span></a>
+              </div>
 
+            
          {/* nav items for large devices */}
-         <ul className="md:flex space-x-12 hidden">
+         <div className='flex'>
+         <ul className="md:flex space-x-12 hidden mr-12">
              {
              navItems.map(({link, path}) => <Link key={path} spy={true} smooth={true} offset={-100} to={path} className="block cursor-pointer text-base text-grey900 hover:text-brandPrimary first:font-medium">{link}</Link> )
              }
          </ul>
+         <Dropdown/>
+         </div>
           
+       
+
           {/* btn for large devices */}
           <div className="space-x-12 hidden lg:flex items-center">
               <a href="/" className="hidden lg:flex items-center text-brandPrimary hover:text-grey900">Login</a>
               <Button type="button" title='Sign up' variant='bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey'/>
-              {/* <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey">Sign up</button> */}
           </div>
 
           {/* menu btn for only mobile devices */}
