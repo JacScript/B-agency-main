31daef6cd5e51300640f55554c93147e04a70ba4
JacScript <jacksonsylas59@gmail.com>
2024-05-29 22:34:20 +0300

    minor changes

--- src/components/Menumarket.jsx
+++ src/components/Menumarket.jsx
@@ -6,7 +6,7 @@ const Menumarket = ({items}) => {
   return (
     <div className="w-4/5 mx-auto md:px-24 md:h-80 md:flex md:flex-row md:mt-0 mt-12 justify-between items-center flex flex-col">
       {
-        <div className="grid grid-cols-3 md:flex w-full h-80 md:h-56 justify-between">
+        <div className="grid grid-cols-3 md:flex w-full h-80 md:h-56 justify-between" >
           {items.map((item, id) => (
             <div className="w-full h-20 md:w-40 md:h-56">
               <a className="cursor-pointer flex flex-col items-center">
